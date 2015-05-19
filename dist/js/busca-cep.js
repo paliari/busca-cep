3ed2b6718b0bfab9838e1deedd3eef71872ebe8c
Marcos Paliari <marcos@paliari.com.br>
2015-05-19 10:07:55 -0300

    gulp compile

--- dist/js/busca-cep.js
+++ dist/js/busca-cep.js
@@ -1,93 +1,88 @@
 (function() {
-  window.BuscaCep = (function() {
-    BuscaCep.init = function(url, format) {
-      window.BuscaCep.URL = url || '/api/address/cep/:cep.json';
-      return window.BuscaCep.format = format || {
-        cidade_id: 'city.id',
-        cidade: 'city',
-        cidade_nome: 'city.nome',
-        cidade_uf: 'city.uf',
-        logradouro: 'street.nome',
-        bairro: 'neighborhood.nome',
-        mensagem: 'message',
-        resultado: 'resultado'
-      };
-    };
-
-    function BuscaCep($el) {
-      this.$el = $el;
-      this._url = window.BuscaCep.URL;
-      this.find('[data-endereco-cep]').on('blur', (function(_this) {
-        return function(e) {
-          return _this.get(e.target.value);
-        };
-      })(this));
-    }
-
-    BuscaCep.prototype.find = function(s) {
-      return this.$el.find(s);
-    };
-
-    BuscaCep.prototype.get = function(cep) {
-      var url;
-      cep = cep.replace(/\D/g, '');
-      url = this._url.replace(':cep', cep);
-      if (cep) {
-        this.setMessage();
-        return $.get(url, (function(_this) {
-          return function(data) {
-            return _this.populate(data);
+  (function($) {
+    var BuscaCep;
+    BuscaCep = (function() {
+      function BuscaCep($el, options) {
+        this.$el = $el;
+        this._url = options.url;
+        this.returnConverter = options.returnConverter;
+        this.find('[data-endereco-cep]').on('blur', (function(_this) {
+          return function(e) {
+            return _this.getCep(e.target.value);
           };
         })(this));
       }
-    };
 
-    BuscaCep.prototype.populate = function(data) {
-      data = this.convertData(data);
-      console.log(data);
-      this.find('[data-endereco-cidade-id]').val(data.cidade_id);
-      this.find('[data-endereco-cidade]').val(data.cidade_nome + '-' + data.cidade_uf);
-      this.find('[data-endereco-logradouro]').val(data.logradouro);
-      this.find('[data-endereco-bairro]').val(data.bairro);
-      if (Number(data.resultado) !== 1) {
-        return this.setMessage(data.mensagem);
-      }
-    };
+      BuscaCep.prototype.find = function(s) {
+        return this.$el.find(s);
+      };
 
-    BuscaCep.prototype.setMessage = function(msg) {
-      var el;
-      el = this.find('[data-endereco-message]');
-      if (msg) {
-        return el.addClass('alert alert-warning').html(msg);
-      } else {
-        return el.removeClass('alert alert-warning').html('');
-      }
-    };
+      BuscaCep.prototype.setVal = function(selector, value) {
+        return this.find(selector).val(value);
+      };
 
-    BuscaCep.prototype.convertData = function(data) {
-      var k, ret, v;
-      ret = {};
-      for (k in window.BuscaCep.format) {
-        v = this.refObj(data, BuscaCep.format[k]);
-        ret[k] = v;
-      }
-      return ret;
-    };
-
-    BuscaCep.prototype.refObj = function(obj, str) {
-      var i, j, len, ref;
-      ref = str.split('.');
-      for (j = 0, len = ref.length; j < len; j++) {
-        i = ref[j];
-        if (obj) {
-          obj = obj[i];
+      BuscaCep.prototype.getCep = function(cep) {
+        var url;
+        cep = cep.replace(/\D/g, '');
+        url = this._url.replace(':cep', cep);
+        if (cep) {
+          this.setMessage();
+          return $.get(url, (function(_this) {
+            return function(data) {
+              return _this.populate(data);
+            };
+          })(this));
         }
-      }
-      return obj;
+      };
+
+      BuscaCep.prototype.populate = function(data) {
+        data = this.convertData(data);
+        console.log(data);
+        this.setVal('[data-endereco-cidade-id]', data.city.id);
+        this.setVal('[data-endereco-cidade]', data.city.nome + '-' + data.city.uf);
+        this.setVal('[data-endereco-logradouro]', data.street.nome);
+        this.setVal('[data-endereco-bairro]', data.neighborhood.nome);
+        if (Number(data.resultado) !== 1) {
+          return this.setMessage(data.mensagem);
+        }
+      };
+
+      BuscaCep.prototype.setMessage = function(msg) {
+        var el;
+        el = this.find('[data-endereco-message]');
+        if (msg) {
+          return el.addClass('alert alert-warning').html(msg);
+        } else {
+          return el.removeClass('alert alert-warning').html('');
+        }
+      };
+
+      return BuscaCep;
+
+    })();
+    $.buscaCep = function(el, options) {
+      var base;
+      base = this;
+      base.$el = $(el);
+      base.el = el;
+      base.$el.data("buscaCep", base);
+      base.init = function() {
+        base.options = $.extend({}, $.buscaCep.defaultOptions, options);
+        return new BuscaCep(base.$el, base.options);
+      };
+      return base.init();
     };
-
-    return BuscaCep;
-
-  })();
+    $.buscaCep.defaultOptions = {
+      url: '/api/address/cep/:cep.json',
+      returnConverter: function(data) {
+        return data;
+      }
+    };
+    return $.fn.buscaCep = function(options) {
+      return this.each(function() {
+        return new $.buscaCep(this, options);
+      });
+    };
+  })(jQuery);
 
 }).call(this);
